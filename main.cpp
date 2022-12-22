29147db28280b1969f22e26fe4ffac5ca8e53f81
Rostislav <rostislav.lysenkov24@gmail.com>
2022-12-23 00:32:00 +0300

    add IsLoggedIn()

--- main.cpp
+++ main.cpp
@@ -4,6 +4,25 @@
 
 using namespace std;
 
-bool IsLoggedIn(){
+bool IsLoggedIn()
+{
+    string username, password, un, pw;
+
     cout << "Enter username: ";
+    cin >> username;
+    cout << "Enter password: ";
+    cin >> password;
+
+    ifstream read("data\\" + username + ".txt");
+    getline(read,un);   // read username
+    getline(read, pw);  // read password
+
+    if (un == username && pw == password)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
