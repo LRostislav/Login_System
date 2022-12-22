70666ea0a7d06940b842a4546dcd22837fa1d426
Rostislav <rostislav.lysenkov24@gmail.com>
2022-12-23 00:42:24 +0300

    add main()

--- main.cpp
+++ main.cpp
@@ -26,3 +26,47 @@ bool IsLoggedIn()
         return false;
     }
 }
+
+int main()
+{
+    int choice;
+
+    cout << "1: Register\n2: Login\nYour choice: ";
+    cin >> choice;
+
+    // Register
+    if (choice == 1)
+    {
+        string username, password;
+
+        cout << "select a username";
+        cin >> username;
+
+        cout << "select a password";
+        cin >> password;
+
+        ofstream file;
+        file.open("data\\" + username + ".txt");
+        file << username << endl << password;
+        file.close();
+
+        main();
+    }
+    else if (choice == 2)
+    {
+        bool status = IsLoggedIn();
+
+        if (!status)
+        {
+            cout << "False Login!" << endl;
+            system("PAUSE");
+            return 0;
+        }
+        else
+        {
+            cout << "Succesfully logged in!" << endl;
+            system("PAUSE");
+            return 1;
+        }
+    }
+}
